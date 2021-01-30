8a9eb781518acca9e6ceee76fbc3ab728317f91f
Marcin Krysiński <marcin_krysinski@outlook.com>
2021-01-30 11:44:47 +0100

    Change format of logging

--- src/services/winston.ts
+++ src/services/winston.ts
@@ -1,10 +1,14 @@
 import winston, { LoggerOptions, format } from 'winston';
 
-const consoleTransport = new winston.transports.Console();
+const consoleTransport = new winston.transports.Console({
+  format: format.simple(),
+});
+
 const errorLogsFileTransport = new winston.transports.File({
   filename: 'error.log',
   level: 'error',
 });
+
 const allLogsFileTransport = new winston.transports.File({
   filename: 'all_logs.log',
   level: 'info',
